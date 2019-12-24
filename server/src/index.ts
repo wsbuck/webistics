2cfdd5baae77e984c43fc14204dbb93fd5f9053e
William Buck <wsbuckiii@gmail.com>
2019-12-23 18:00:01 -0800

    Added local development server
    * commands `npm run start` starts local development server
    * need to run `sls dynamodb start` for local dynamodb table as well

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,16 +3,40 @@ import bodyParser from 'body-parser';
 import express from 'express';
 import AWS from 'aws-sdk';
 import uuid from 'uuid/v1';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+
 const ANALYTICS_TABLE = process.env.DYNAMODB_TABLE;
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const IS_OFFLINE = process.env.IS_OFFLINE;
+
+let dynamoDb: AWS.DynamoDB.DocumentClient;
+
+if (IS_OFFLINE) {
+  dynamoDb = new AWS.DynamoDB.DocumentClient({
+    region: 'localhost',
+    endpoint: 'http://localhost:8000'
+  });
+} else {
+  dynamoDb = new AWS.DynamoDB.DocumentClient();
+}
 
 interface AnalyticsPostBody {
   ipAddress: string;
 }
 
 app.use(bodyParser.json({ strict: false }));
+app.use(function (req, res, next) {
+  // res.header('Access-Control-Allow-Origin', process.env.MY_DOMAIN);
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-type, Accept'
+  );
+  next();
+});
 
 // Create POST Analytics endpoint
 app.post('/analytics', function (req, res) {
@@ -49,5 +73,9 @@ app.get('/analytics', function (req, res) {
     res.status(200).json(data.Items)
   });
 });
+
+if (IS_OFFLINE) {
+  app.listen(8080, () => console.log('Listening on port 8080'));
+}
 
 module.exports.handler = sls(app);
