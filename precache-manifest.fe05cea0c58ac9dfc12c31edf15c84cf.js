d63de9f53ac5a26e9c5e579ab49241f010e95b57
William Buck <wsbuckiii@gmail.com>
2020-01-16 23:36:14 -0800

    Updates

---
+++ precache-manifest.fe05cea0c58ac9dfc12c31edf15c84cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e692451c5ccaff05d52b42f07683a1d",
+    "url": "/webistics/index.html"
+  },
+  {
+    "revision": "9982b648743206eb886d",
+    "url": "/webistics/static/css/main.3c20007a.chunk.css"
+  },
+  {
+    "revision": "cd6805606b58d68ac534",
+    "url": "/webistics/static/js/2.a510d499.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/webistics/static/js/2.a510d499.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9982b648743206eb886d",
+    "url": "/webistics/static/js/main.186b20cf.chunk.js"
+  },
+  {
+    "revision": "f9a24a4c5955ab43212b",
+    "url": "/webistics/static/js/runtime-main.31d99e1a.js"
+  }
+]);
